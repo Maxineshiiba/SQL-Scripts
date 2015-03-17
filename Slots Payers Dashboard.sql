a8a629a684dd818a509f6a46dba82e1682c033b1
Maxineshiiba <maggiemilan@gmail.com>
2015-03-17 15:51:51 -0700

    Create Slots Payers Dashboard.sql
    I need pay attention to the way of using count distinct in the query

---
+++ Slots Payers Dashboard.sql
@@ -0,0 +1,15 @@
+----Slots Payers Dashboard
+select 
+case
+when platform ilike '%i%' then 'iOS'
+when platform ilike '%a%' then 'Android'
+when platform ilike '%z%' then 'Amazon'
+else 'Other'
+end as platform
+,date(install_cohort) as dte
+,source_table
+,count(distinct(case when is_payer = 1 then user_id else null end)) as payers
+,count(distinct(user_id)) as all_users
+from 
+models.slots_user_summary
+group by 1,2,3
